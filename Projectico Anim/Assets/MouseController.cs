90d72b0e396ed858121f58e4d6aad1eee1bdd965
s4ru <davidriveracordo@hotmail.com>
2023-10-06 18:51:12 -0500

    vamonos

--- Projectico Anim/Assets/MouseController.cs
+++ Projectico Anim/Assets/MouseController.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine.Utility;
 using UnityEngine;
 
 public class MouseController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Camera Cam;
+    Vector3 pos;
+    public float offset = 2f;
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        pos = Input.mousePosition;
+        pos.z = offset;
+        transform.position = Cam.ScreenToWorldPoint(pos);
     }
 }
